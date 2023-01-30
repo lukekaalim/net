0e85d9263a8fd5148435c2590f8b03a1b1e287b2
Luke Kaalim <lukekaalim@gmail.com>
2023-01-30 15:23:36 +1100

    npm install to apply lock changes

--- postversion.sh
+++ postversion.sh
@@ -4,6 +4,8 @@ name=$(cat package.json | jq .name -r)
 version=$(cat package.json | jq .version -r)
 tag="$name@$version"
 
+(cd ..; npm i;)
+
 git add .
 git commit -m $tag
 git tag -a $tag -m "Publied version $version of $name"
